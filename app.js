5e7ae7d217bd8b9850ac6b29d6da32c67f8c8b9c
Rodrigo Llanos <rodrigo.llanosordonez@altran.com>
2018-05-22 10:15:53 +0200

    [FEAT] First commit. Listenning and connections

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const monggoose = require('mongoose');
+
+const app = express();
+
+monggoose.connection.openUri(
+  'mongodb://localhost:27017/hospitalDB',
+  (err, res) => {
+    if (err) throw err;
+
+    console.log('MongoDB database online!');
+  }
+);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: 'Success request',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Express server running on port 3000');
+});
