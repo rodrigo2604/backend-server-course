69ac7a80f65d35183d6b7c7e9ec59a687c255661
Rodrigo Llanos <rodrigo.llanosordonez@altran.com>
2018-05-22 17:28:46 +0200

    [FEAT] Global searching

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,7 @@ const app = express();
 
 const Hospital = require('../models/hospital');
 const Doctor = require('../models/doctor');
+const User = require('../models/user');
 
 app.get('/all/:search', (req, res) => {
   const search = req.params.search;
@@ -12,27 +13,24 @@ app.get('/all/:search', (req, res) => {
   const promises = [
     Hospital.find({ name: searchRegex }),
     Doctor.find({ name: searchRegex }),
+    User.find({ name: searchRegex }),
   ];
 
-  Promise.all(promises).then(([hospitals, doctors]) =>
-    res.status(200).json({
-      hospitals,
-      doctors,
-    })
-  );
-  /*.catch(err =>
+  Promise.all(promises)
+    .catch(err =>
       res.status(500).json({
-        ok: true,
-        message: 'Error on search hospitals',
+        ok: false,
+        mensaje: 'Error on global search',
         errors: err,
       })
     )
-    .then(hospitals =>
+    .then(([hospitals, doctors, users]) =>
       res.status(200).json({
-        ok: true,
         hospitals,
+        doctors,
+        users,
       })
-    );*/
+    );
 });
 
 module.exports = app;
